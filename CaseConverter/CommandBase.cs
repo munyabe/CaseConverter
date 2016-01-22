27268937521c12d45e2369d0b2b08a042dc11a4a
munyabe <yo.betek@gmail.com>
2016-01-22 19:16:04 +0900

    Divide a command class in abstract and concrete classes.

---
+++ CaseConverter/CommandBase.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace CaseConverter
+{
+    /// <summary>
+    /// 拡張機能として登録するコマンドの基底クラスです。
+    /// </summary>
+    internal abstract class CommandBase
+    {
+        /// <summary>
+        /// コマンドを提供するパッケージです。
+        /// </summary>
+        private readonly Package _package;
+
+        /// <summary>
+        /// シングルトンのインスタンスを取得します。
+        /// </summary>
+        public static ConvertCaseCommand Instance { get; protected set; }
+
+        /// <summary>
+        /// サービスプロバイダーを取得します。
+        /// </summary>
+        protected IServiceProvider ServiceProvider
+        {
+            get { return _package; }
+        }
+
+        /// <summary>
+        /// インスタンスを初期化します。
+        /// </summary>
+        /// <remarks>
+        /// コマンドは .vsct ファイルに定義されている必要があります。
+        /// </remarks>
+        /// <param name="package">コマンドを提供するパッケージ</param>
+        /// <param name="commandId">コマンドのID</param>
+        /// <param name="commandSetId">コマンドメニューグループのID</param>
+        protected CommandBase(Package package, int commandId, Guid commandSetId)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            _package = package;
+
+            var commandService = ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(commandSetId, commandId);
+                var menuItem = new MenuCommand(MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// コマンドを実行します。
+        /// </summary>
+        protected abstract void Execute();
+
+        /// <summary>
+        /// コマンドを実行した際のコールバックです。
+        /// </summary>
+        /// <param name="sender">イベントの発行者</param>
+        /// <param name="e">イベント引数</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            ShowMessage();
+            Execute();
+        }
+
+        /// <summary>
+        /// メッセージを表示します。
+        /// </summary>
+        private void ShowMessage()
+        {
+            var message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", GetType().FullName);
+            var title = "Execute Command";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                ServiceProvider,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
--- CaseConverter/ConvertCaseCommand.cs
+++ CaseConverter/ConvertCaseCommand.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.ComponentModel.Design;
-using System.Globalization;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 
 namespace CaseConverter
 {
     /// <summary>
     /// 文字列をキャメルケース⇔スネークケースに変換するコマンドです。
     /// </summary>
-    internal sealed class ConvertCaseCommand
+    internal sealed class ConvertCaseCommand : CommandBase
     {
         /// <summary>
         /// コマンドのIDです。
@@ -22,51 +19,11 @@ namespace CaseConverter
         public static readonly Guid CommandSet = new Guid("f038e966-3a02-4eef-bfad-cd8fab3c4d6d");
 
         /// <summary>
-        /// コマンドを提供するパッケージです。
-        /// </summary>
-        private readonly Package package;
-
-        /// <summary>
         /// インスタンスを初期化します。
-        /// コマンドは .vsct ファイルに定義されている必要があります。
         /// </summary>
         /// <param name="package">コマンドを提供するパッケージ</param>
-        private ConvertCaseCommand(Package package)
+        private ConvertCaseCommand(Package package) : base(package, CommandId, CommandSet)
         {
-            if (package == null)
-            {
-                throw new ArgumentNullException("package");
-            }
-
-            this.package = package;
-
-            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-            if (commandService != null)
-            {
-                var menuCommandID = new CommandID(CommandSet, CommandId);
-                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
-                commandService.AddCommand(menuItem);
-            }
-        }
-
-        /// <summary>
-        /// シングルトンのインスタンスです。
-        /// </summary>
-        public static ConvertCaseCommand Instance
-        {
-            get;
-            private set;
-        }
-
-        /// <summary>
-        /// サービスプロバイダーを取得します。
-        /// </summary>
-        private IServiceProvider ServiceProvider
-        {
-            get
-            {
-                return this.package;
-            }
         }
 
         /// <summary>
@@ -78,24 +35,9 @@ namespace CaseConverter
             Instance = new ConvertCaseCommand(package);
         }
 
-        /// <summary>
-        /// コマンドを実行します。
-        /// </summary>
-        /// <param name="sender">イベントの発行者</param>
-        /// <param name="e">イベント引数</param>
-        private void MenuItemCallback(object sender, EventArgs e)
+        /// <inheritdoc />
+        protected override void Execute()
         {
-            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
-            string title = "ConvertCaseCommand";
-
-            // Show a message box to prove we were here
-            VsShellUtilities.ShowMessageBox(
-                this.ServiceProvider,
-                message,
-                title,
-                OLEMSGICON.OLEMSGICON_INFO,
-                OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
         }
     }
 }
