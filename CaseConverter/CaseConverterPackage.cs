619060325a1337298177298bdc5ce777d964dd7c
munyabe <yo.betek@gmail.com>
2016-01-22 18:18:15 +0900

    Delete unnecessary codes.

--- CaseConverter/CaseConverterPackage.cs
+++ CaseConverter/CaseConverterPackage.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.ComponentModel.Design;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Runtime.InteropServices;
-using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.Win32;
 
 namespace CaseConverter
 {
@@ -17,7 +10,7 @@ namespace CaseConverter
     /// </summary>
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Visual Studio のヘルプ/バージョン情報に表示される情報です。
-    [Guid(CaseConverterPackage.PackageGuidString)]
+    [Guid(PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class CaseConverterPackage : Package
@@ -28,19 +21,6 @@ namespace CaseConverter
         public const string PackageGuidString = "3293cb25-75b9-4d5a-a248-ea3cf25fc4c8";
 
         /// <summary>
-        /// インスタンスを初期化します。
-        /// </summary>
-        public CaseConverterPackage()
-        {
-            // Inside this method you can place any initialization code that does not require
-            // any Visual Studio service because at this point the package object is created but
-            // not sited yet inside Visual Studio environment. The place to do all the other
-            // initialization is the Initialize method.
-        }
-
-        #region Package Members
-
-        /// <summary>
         /// パッケージを初期化します。
         /// </summary>
         protected override void Initialize()
@@ -48,7 +28,5 @@ namespace CaseConverter
             base.Initialize();
             ConvertCaseCommand.Initialize(this);
         }
-
-        #endregion
     }
 }
