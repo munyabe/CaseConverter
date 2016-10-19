c924816d8b648be2dd504b3645874bfe90d531c8
munyabe <yo.betek@gmail.com>
2016-10-19 18:30:45 +0900

    Change option labels.

---
+++ CaseConverter/Options/PatternOption.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using CaseConverter.Converters;
+
+namespace CaseConverter.Options
+{
+    /// <summary>
+    /// 文字列の変換パターンのオプションです。
+    /// </summary>
+    /// <remarks>
+    /// オプション画面での表記をカスタマイズするために定義したクラスです。
+    /// </remarks>
+    public class PatternOption
+    {
+        /// <summary>
+        /// 文字列の変換パターンを取得または設定します。
+        /// </summary>
+        [Category("Basic")]
+        [DisplayName("Pattern")]
+        [TypeConverter(typeof(StringCasePatternConverter))]
+        public StringCasePattern Pattern { get; set; }
+
+        /// <inheritdoc />
+        /// <remarks>
+        /// オプション画面の表記を変更するためにオーバーライドします。
+        /// </remarks>
+        public override string ToString()
+        {
+            return StringCasePatternConverter.Instance.ConvertTo(Pattern);
+        }
+    }
+}
